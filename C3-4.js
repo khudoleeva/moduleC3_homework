016fc9263c783d96a75fcd8a28da6fd6c356b767
khudoleeva <109233532+khudoleeva@users.noreply.github.com>
2022-10-07 17:14:00 +0800

    Update C3-4.js

--- C3-4.js
+++ C3-4.js
@@ -1,42 +1,40 @@
-class ElectricalApp {
-	constructor(name){
-		this.name = name;
-		this.included = false;
-	}
-	inclusion(i){
-		if (i == 'on') {
-		alert("прибор включен");
+
+function ElectricalApp(name){
+	this.name = name;
+	this.included = false;
+}
+ElectricalApp.prototype.inclusion = function(i){
+	if (i == 'on') {
+		console.log("прибор включен");
 		this.included = true;
 	}else if (i == 'off'){
-		alert("прибор выключен");
-		this.included = false;
-	}
-	}
-}
-
-class Lamp extends ElectricalApp {
-	constructor(name, brend, power){
-		super(name);
-		this.brend = brend;
-		this.power = power;
+		console.log("прибор выключен");
 		this.included = false;
 	}
 }
-
-class Comp extends ElectricalApp {
-	constructor(name, brend, power, type){
-		super(name);
-		this.brend = brend;
-		this.power = power;
-		this.included = true;
-	}
+function Lamp(name, brend, power){
+	this.name = name;
+	this.brend = brend;
+	this.power = power;
+	this.included = false;
 }
+Lamp.prototype = new ElectricalApp();
+function Comp(name, brend, power, type){
+	this.name = name;
+	this.brend = brend;
+	this.power = power;
+	this.included = true;
+	this.type = type;
+}
+Comp.prototype = new ElectricalApp();
 const lamp1 = new Lamp('lamp', 'samsung', 120);
 const comp1 = new Comp("computer", 'aser',220, "notebook");
 lamp1.inclusion("on");
 for (let key in lamp1){
-	alert(key +" "+lamp1.key);
+	console.log(key + " " + lamp1[key]);
 }
 
-// comp1.inclusion("off");
-// alert(comp1.included);
+comp1.inclusion("off");
+for (let key in comp1){
+	console.log(key + " " + comp1[key]);
+}
