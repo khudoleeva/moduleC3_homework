910eb754aca9fff90ef3cffe2413cba20c47f977
khudoleeva <109233532+khudoleeva@users.noreply.github.com>
2022-10-07 17:11:20 +0800

    Add files via upload

---
+++ C3-5.js
@@ -0,0 +1,44 @@
+class ElectricalApp {
+	constructor(name){
+		this.name = name;
+		this.included = false;
+	}
+	inclusion(i){
+		if (i == 'on') {
+		console.log("прибор включен");
+		this.included = true;
+	}else if (i == 'off'){
+		console.log("прибор выключен");
+		this.included = false;
+	}
+	}
+}
+
+class Lamp extends ElectricalApp {
+	constructor(name, brend, power){
+		super(name);
+		this.brend = brend;
+		this.power = power;
+		this.included = false;
+	}
+}
+
+class Comp extends ElectricalApp {
+	constructor(name, brend, power, type){
+		super(name);
+		this.brend = brend;
+		this.power = power;
+		this.included = true;
+	}
+}
+const lamp1 = new Lamp('lamp', 'samsung', 120);
+const comp1 = new Comp("computer", 'aser',220, "notebook");
+lamp1.inclusion("on");
+for (let key in lamp1){
+	console.log(key + " " + lamp1[key]);
+}
+
+comp1.inclusion("off");
+for (let key in comp1){
+	console.log(key + " " + comp1[key]);
+}
